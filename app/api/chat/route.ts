ef906f5748aa324e0d762a328a2a6889ca1c3c8a
elsherbiny <moellsherbiny@gmail.com>
2024-08-23 11:06:19 +0300

    change model

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -38,7 +38,7 @@ export async function POST(request: NextRequest) {
     );
 
   const model = genAI.getGenerativeModel({
-    model: "gemini-1.5-pro",
+    model: "gemini-1.5-flash",
     generationConfig,
     safetySettings,
   });
