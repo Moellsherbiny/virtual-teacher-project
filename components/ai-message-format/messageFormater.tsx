d76a8bd17cfb8f40602951f6e26e810b95e698ef
moellsherbiny <moellsherbiny@gmail.com>
2025-04-12 14:29:29 +0200

    change style

--- components/ai-message-format/messageFormater.tsx
+++ components/ai-message-format/messageFormater.tsx
@@ -1,6 +1,5 @@
 import Markdown from "react-markdown";
 import SyntaxHighlighter from "react-syntax-highlighter";
-import { dracula } from "react-syntax-highlighter/dist/esm/styles/hljs";
 import remarkGfm from "remark-gfm";
 
 const CodeBlock = ({
@@ -14,7 +13,6 @@ const CodeBlock = ({
     <div dir="ltr" className="code-block-wrapper">
       <SyntaxHighlighter
         language={language}
-        style={dracula}
         customStyle={{
           maxWidth: "100%",
           overflowX: "auto",
